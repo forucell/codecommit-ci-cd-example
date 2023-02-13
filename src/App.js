647ca5c032a847e3d58c290b758a8fdbfecc3217
forucell <saydiolim.ganiev@gmail.com>
2023-02-13 14:04:09 +0900

    App.js changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New CodeCommit CI/CD !!!
+          Github CodePipeline CI/CD !!!!
         </a>
       </header>
     </div>
